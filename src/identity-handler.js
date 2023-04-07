054d85d085902c8e9b2f8facebf14bf50b623aeb
shwangMP <shwang@mparticle.com>
2023-04-07 10:14:28 -0500

    Update docs links (#36)

--- src/identity-handler.js
+++ src/identity-handler.js
@@ -2,9 +2,9 @@
 The 'mParticleUser' is an object with methods get user Identities and set/get user attributes
 Partners can determine what userIds are available to use in their SDK
 Call mParticleUser.getUserIdentities() to return an object of userIdentities --> { userIdentities: {customerid: '1234', email: 'email@gmail.com'} }
-For more identity types, see http://docs.mparticle.com/developers/sdk/javascript/identity#allowed-identity-types
+For more identity types, see https://docs.mparticle.com/developers/sdk/web/idsync/#supported-identity-types
 Call mParticleUser.getMPID() to get mParticle ID
-For any additional methods, see http://docs.mparticle.com/developers/sdk/javascript/apidocs/classes/mParticle.Identity.getCurrentUser().html
+For any additional methods, see https://docs.mparticle.com/developers/sdk/web/core-apidocs/classes/mParticle.Identity.getCurrentUser().html
 */
 
 /*
@@ -15,7 +15,7 @@ identityApiRequest has the schema:
     email: 'abc'
   }
 }
-For more userIdentity types, see http://docs.mparticle.com/developers/sdk/javascript/identity#allowed-identity-types
+For more userIdentity types, see https://docs.mparticle.com/developers/sdk/web/idsync/#supported-identity-types
 */
 
 function IdentityHandler(common) {
